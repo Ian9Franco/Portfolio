388dac87ea7978eef0488639e91a9f8d0be993ae
Ian Franco <ianfranco9ive.com>
2024-09-10 09:57:27 -0300

    .gitignore

--- js/copybutton.js
+++ js/copybutton.js
@@ -1,30 +1,30 @@
-const e = document.getElementById("email");
-const o = document.getElementById("discord");
-const i = document.getElementById("button-copy-email");
-const n = document.getElementById("button-copy-discord");
+const emailTeams = document.getElementById("teams-email");
+const emailContacto = document.getElementById("email");
+const buttonCopyTeams = document.getElementById("button-copy-teams");
+const buttonCopyEmail = document.getElementById("button-copy-email");
 
-i?.addEventListener("click", () => {
-  if (e) {
-    navigator?.clipboard.writeText(e.value);
-    let t = document.getElementById("tooltip-email");
-    t.innerHTML = "Copiado";
+buttonCopyTeams?.addEventListener("click", () => {
+  if (emailTeams) {
+    navigator?.clipboard.writeText(emailTeams.value);
+    let tooltipTeams = document.getElementById("tooltip-teams");
+    tooltipTeams.innerHTML = "Copiado";
   }
 });
 
-n?.addEventListener("click", () => {
-  if (o) {
-    navigator?.clipboard.writeText(o.value);
-    let t = document.getElementById("tooltip-discord");
-    t.innerHTML = "Copiado";
+buttonCopyEmail?.addEventListener("click", () => {
+  if (emailContacto) {
+    navigator?.clipboard.writeText(emailContacto.value);
+    let tooltipEmail = document.getElementById("tooltip-email");
+    tooltipEmail.innerHTML = "Copiado";
   }
 });
 
-i?.addEventListener("mouseleave", () => {
-  let t = document.getElementById("tooltip-email");
-  t.innerHTML = "Copiar";
+buttonCopyTeams?.addEventListener("mouseleave", () => {
+  let tooltipTeams = document.getElementById("tooltip-teams");
+  tooltipTeams.innerHTML = "Copiar";
 });
 
-n?.addEventListener("mouseleave", () => {
-  let t = document.getElementById("tooltip-discord");
-  t.innerHTML = "Copiar";
+buttonCopyEmail?.addEventListener("mouseleave", () => {
+  let tooltipEmail = document.getElementById("tooltip-email");
+  tooltipEmail.innerHTML = "Copiar email";
 });
